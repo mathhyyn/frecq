df6e3bbcc693227268f6bc5a3d3501c58be1e029
anna <annyshka20072002@gmail.com>
2023-10-09 20:19:03 +0300

    list of users

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,50 @@
+/*import 'package:flutter/material.dart';
+import 'widgets/user.dart';
+import 'widgets/users_list.dart';*/
+/*
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('main page')),
+      body: Center(
+          child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const UsersListPage()));
+              },
+              child: const Text('open users list'))),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes: {
+      '/': (BuildContext context) => const MainScreen(),
+      '/list': (BuildContext context) => const UsersListPage(),
+      '/user': (BuildContext context) => const SecondScreen(-1)
+    },
+    onGenerateRoute: (routeSettings) {
+      var path = routeSettings.name?.split('/');
+
+      if (path?[1] == "user") {
+        return MaterialPageRoute(
+          builder: (context) => const SecondScreen(1/*_id: path[2]*/),
+          settings: routeSettings,
+        );
+      }
+    },
+  ));
+}
+*/
+
 import 'package:flutter/material.dart';
 import 'widgets/users_list.dart';
 
--- lib/widgets/users_list.dart
+++ lib/widgets/users_list.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'user.dart';
 import 'image.dart';
 
+const List<int> users = <int>[1, 2];
+
 class UsersListPage extends StatelessWidget {
   const UsersListPage({super.key});
 
@@ -9,17 +11,23 @@ class UsersListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('users list page')),
-      body: Column( children: [
-        TextButton(onPressed: (){
-        Navigator.pop(context);
-      }, child: const Text('back')),
-        TextButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondScreen(1)));
-        }, child: const Text('user page')),
-        TextButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondScreen(5)));
-        }, child: const Text('image widget'))
-      ]),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: users.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SecondScreen(users[index])));
+                    },
+                    child: Text('user page $index'))
+              ],
+            );
+          }),
     );
   }
 }
